e2e65e57974d26d53cf0d76bf518ef2b74ff4926
blinkul <anghelciprian91@gmail.com>
2018-06-05 23:32:12 +0300

    https://github.com/blinkul/guess-the-number-game.git

--- core/src/main/java/academy.learnprogramming/Main.java
+++ core/src/main/java/academy.learnprogramming/Main.java
@@ -4,22 +4,21 @@ package academy.learnprogramming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     public static final Logger log = LoggerFactory.getLogger(Main.class);
 
-    public static final String CONFIG_LOCATION = "beans.xml";
-
     public static void main(String[] args) {
         log.info("Guess The Number Game");
 
         //Create context (container)
-        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
         //Get the NumberGenerator bean from the context
-        NumberGenerator numberGenerator = context.getBean("numberGenerator", NumberGenerator.class);
+        NumberGenerator numberGenerator = context.getBean(NumberGenerator.class);
 
         //get random number
         int number = numberGenerator.next();
