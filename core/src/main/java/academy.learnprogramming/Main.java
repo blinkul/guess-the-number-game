112a38cf61222aa91719e1431500929828d1c693
blinkul <anghelciprian91@gmail.com>
2018-06-05 23:33:01 +0300

    https://github.com/blinkul/guess-the-number-game.git

--- core/src/main/java/academy.learnprogramming/Main.java
+++ core/src/main/java/academy.learnprogramming/Main.java
@@ -5,11 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
-    public static final Logger log = LoggerFactory.getLogger(Main.class);
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         log.info("Guess The Number Game");
